c7c9dfe694acf48785acddb12f6bf7624a2988ec
dinesh7090 <dinesh7090@outlook.com>
2026-02-26 08:24:15 +0530

    Completed AI

---
+++ labs/Python/AI/AI_Chat.py
@@ -0,0 +1,19 @@
+#pip3 install openai
+import os
+from dotenv import load_dotenv
+from openai import AzureOpenAI
+client = AzureOpenAI (azure_endpoint="https://zix-azure-openai.openai.azure.com/",
+api_key = "",
+api_version = "2025-04-01-preview")
+print("Welcome To ChatBoat!")
+while True:
+    msg = input("You: ")
+    if msg == 'quit':
+        break
+    response = client.chat.completions.create(
+        #model = 'gpt-5-mini',
+        model = 'gpt-5.1-chat',
+        messages = [{"role":"user", "content": msg}],
+        max_completion_tokens=500
+    )
+    print(f"Chatbot: {response.choices[0].message.content}\n")
